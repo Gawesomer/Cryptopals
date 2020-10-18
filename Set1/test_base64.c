40465e0199b3a694ffa5c184846ee8b4166c336d
Jean-Baptiste Jouband <jj.jouband@gmail.com>
2020-10-17 18:50:04 -0600

    Fix off by one error

--- Set1/test_base64.c
+++ Set1/test_base64.c
@@ -9,7 +9,7 @@
 void assert_bytes_eq(size_t expectedlen, uint8_t *expected, uint8_t *actual)
 {
 	int i;
-	for (i = 0; i < expectedlen-1; ++i)
+	for (i = 0; i < expectedlen; ++i)
 		assert(actual[i] == expected[i]);
 }
 
