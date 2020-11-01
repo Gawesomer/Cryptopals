fcd44f5cdfe3f758d036c89f763a2d4e27dc3cd3
Jean-Baptiste Jouband <jj.jouband@gmail.com>
2020-11-01 15:42:05 -0600

    Move ENGLISH_LETTER_FREQ to C-file and usee extern in header

--- include/freq.h
+++ include/freq.h
@@ -6,37 +6,7 @@
 #include <float.h>
 #include <math.h>
 
-/* English letter frequency based on a sample of 40,000 words obtained from:
- * http://pi.math.cornell.edu/~mec/2003-2004/cryptography/subs/frequencies.html
- */
-const float ENGLISH_LETTER_FREQ[26] = {
-	8.12f,	// A
-	1.49f,	// B
-	2.71f,	// C
-	4.32f,	// D
-	12.02f,	// E
-	2.30f,	// F
-	2.03f,	// G
-	5.92f,	// H
-	7.31f,	// I
-	0.10f,	// J
-	0.69f,	// K
-	3.98f,	// L
-	2.61f,	// M
-	6.95f,	// N
-	7.68f,	// O
-	1.82f,	// P
-	0.11f,	// Q
-	6.02f,	// R
-	6.28f,	// S
-	9.10f,	// T
-	2.88f,	// U
-	1.11f,	// V
-	2.09f,	// W
-	0.17f,	// X
-	2.11f,	// Y
-	0.07f,	// Z
-};
+extern const float ENGLISH_LETTER_FREQ[26];
 
 float *freqmap_from_binary(const uint8_t *bits, size_t size);
 
--- set1/challenge3/freq.c
+++ set1/challenge3/freq.c
@@ -1,5 +1,37 @@
 #include "freq.h"
 
+/* English letter frequency based on a sample of 40,000 words obtained from:
+ * http://pi.math.cornell.edu/~mec/2003-2004/cryptography/subs/frequencies.html
+ */
+const float ENGLISH_LETTER_FREQ[26] = {
+	8.12f,	// A
+	1.49f,	// B
+	2.71f,	// C
+	4.32f,	// D
+	12.02f,	// E
+	2.30f,	// F
+	2.03f,	// G
+	5.92f,	// H
+	7.31f,	// I
+	0.10f,	// J
+	0.69f,	// K
+	3.98f,	// L
+	2.61f,	// M
+	6.95f,	// N
+	7.68f,	// O
+	1.82f,	// P
+	0.11f,	// Q
+	6.02f,	// R
+	6.28f,	// S
+	9.10f,	// T
+	2.88f,	// U
+	1.11f,	// V
+	2.09f,	// W
+	0.17f,	// X
+	2.11f,	// Y
+	0.07f,	// Z
+};
+
 /* Compute character occurence of binary array
  * params:
  * 	- bits: binary array
