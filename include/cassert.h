ed16700aa55fdd09086353d4b162918c62df2c6e
Jean-Baptiste Jouband <jj.jouband@gmail.com>
2020-10-31 20:34:01 -0600

    Add docs and float assertions

--- include/cassert.h
+++ include/cassert.h
@@ -5,6 +5,46 @@
 #include <stdint.h>
 #include <assert.h>
 
-void assert_bytes_eq(size_t expectedlen, const uint8_t *expected, const uint8_t *actual);
+/* Assert that `expected` and `actual` floats are equal
+ * params:
+ * 	- expected: float to compare to
+ * 	- actual: float to be compared
+ * 	- eps: positive epsilon to specify the acceptable range within which
+ * 	       floats may be considered equal
+ * returns:
+ * 	ends execution on failure
+ * 	otherwise nothing
+ */
+void assert_floats_eq(float expected, float actual, float eps);
+
+/* Assert that `expected` and `actual` byte arrays are pairwise equal
+ * params:
+ * 	- expectedlen: length of `expected`
+ * 	- expected: byte array to compare to
+ * 	- actual: byte array to be compared
+ * returns:
+ * 	ends execution on failure
+ * 	otherwise nothing
+ * notes:
+ * 	fails to notice that `actual` may be larger than `expected`
+ */
+void assert_bytes_eq(size_t expectedlen, const uint8_t *expected, \
+		     const uint8_t *actual);
+
+/* Assert that `expected` and `actual` float arrays are pairwise equal
+ * params:
+ * 	- expectedlen: length of `expected`
+ * 	- expected: float array to compare to
+ * 	- actual: float array to be compared
+ * 	- eps: positive epsilon to specify the acceptable range within which
+ * 	       floats may be considered equal
+ * returns:
+ * 	ends execution on failure
+ * 	otherwise nothing
+ * notes:
+ * 	fails to notice that `actual` may be larger than `expected`
+ */
+void assert_farrs_eq(size_t expectedlen, const float *expected, \
+		     const float *actual, float eps);
 
 #endif
--- util/cassert.c
+++ util/cassert.c
@@ -1,9 +1,23 @@
 #include "cassert.h"
 
-void assert_bytes_eq(size_t expectedlen, const uint8_t *expected, const uint8_t *actual)
+void assert_floats_eq(float expected, float actual, float eps)
+{
+	assert((expected-eps) <= actual);
+	assert(actual <= (expected+eps));
+}
+
+void assert_bytes_eq(size_t expectedlen, const uint8_t *expected, \
+		     const uint8_t *actual)
 {
 	int i;
 	for (i = 0; i < expectedlen; ++i)
 		assert(actual[i] == expected[i]);
 }
 
+void assert_farrs_eq(size_t expectedlen, const float *expected,
+		     const float *actual, float eps)
+{
+	int i;
+	for (i = 0; i < expectedlen; ++i)
+		assert_floats_eq(expected[i], actual[i], eps);
+}
