51aedab578f98aa13cd6c6bffacb9253682e8d5d
Jean-Baptiste Jouband <jj.jouband@gmail.com>
2020-11-13 14:00:49 -0600

    Add tests for TEST_TRUE

--- util/tests/test_cassert.c
+++ util/tests/test_cassert.c
@@ -8,6 +8,136 @@
 
 #define BUFFSIZE	1024
 
+/*** TEST_TRUE ***/
+
+void test_TEST_TRUE_true(void)
+{
+	printf("%s\n", __func__);
+
+	assert(TEST_TRUE(1) == 0);
+	assert(TEST_TRUE(INT_MAX) == 0);
+	assert(TEST_TRUE(INT_MIN) == 0);
+}
+
+void test_TEST_TRUE_true_w_expr(void)
+{
+	printf("%s\n", __func__);
+
+	assert(TEST_TRUE(3*4 == 12) == 0);
+	assert(TEST_TRUE(0 != 1) == 0);
+}
+
+void test_TEST_TRUE_false(void)
+{
+	printf("%s\n", __func__);
+
+	FILE *devnull, *tmp;
+
+	// Execute with STDOUT redirected to /dev/null
+	devnull = fopen("/dev/null", "w");
+	tmp = stdout;
+	stdout = devnull;
+
+	// Note: We will still see `assert`'s stacktrace despite the STDOUT
+	// redirection as `assert` prints to STDERR
+	assert(TEST_TRUE(0) != 0);
+
+	stdout = tmp;
+	fclose(devnull);
+}
+
+void test_TEST_TRUE_false_w_expr(void)
+{
+	printf("%s\n", __func__);
+
+	FILE *devnull, *tmp;
+
+	devnull = fopen("/dev/null", "w");
+	tmp = stdout;
+	stdout = devnull;
+
+	assert(TEST_TRUE(3*4 == 13) != 0);
+
+	stdout = tmp;
+	fclose(devnull);
+}
+
+void test_TEST_TRUE_stacktrace(void)
+{
+	printf("%s\n", __func__);
+
+	char *actualstr;
+	char expectedstr[BUFFSIZE];
+	FILE *buffer, *tmp;
+
+	/* args:
+	 * 	- function name
+	 * 	- file name
+	 * 	- execution line number (i.e. when macro is called)
+	 * 	- expressions passed to macro
+	 */
+	snprintf(expectedstr, BUFFSIZE, \
+		"==========================================================\n"
+		"FAIL: %s\n"
+		"----------------------------------------------------------\n"
+		"\tFile \"%s\", line %d:\n"
+		"\t\tTEST_TRUE(%s)\n"
+		"\t\t\t`%s` not true\n"
+		"----------------------------------------------------------\n", \
+		__FUNCTION__, __FILE__, __LINE__ + 8, "0", "0");
+	actualstr = calloc(BUFFSIZE, sizeof(char));
+	buffer = fmemopen(actualstr, BUFFSIZE, "w");
+
+	// Execute with STDOUT redirected to `actualstr`
+	tmp = stdout;
+	stdout = buffer;
+
+	TEST_TRUE(0);
+
+	stdout = tmp;
+	fclose(buffer);
+
+	assert(strcmp(actualstr, expectedstr) == 0);
+
+	free(actualstr);
+}
+
+void test_TEST_TRUE_stacktrace_w_expr(void)
+{
+	printf("%s\n", __func__);
+
+	char *actualstr;
+	char expectedstr[BUFFSIZE];
+	FILE *buffer, *tmp;
+
+	snprintf(expectedstr, BUFFSIZE, \
+		"==========================================================\n"
+		"FAIL: %s\n"
+		"----------------------------------------------------------\n"
+		"\tFile \"%s\", line %d:\n"
+		"\t\tTEST_TRUE(%s)\n"
+		"\t\t\t`%s` not true\n"
+		"----------------------------------------------------------\n", \
+		__FUNCTION__, __FILE__, __LINE__ + 7, "0 == 1", "0 == 1");
+	actualstr = calloc(BUFFSIZE, sizeof(char));
+	buffer = fmemopen(actualstr, BUFFSIZE, "w");
+
+	tmp = stdout;
+	stdout = buffer;
+
+	TEST_TRUE(0 == 1);
+
+	stdout = tmp;
+	fclose(buffer);
+
+	assert(strcmp(actualstr, expectedstr) == 0);
+
+	free(actualstr);
+}
+
+
+/*** TEST_INT_EQ ***/
+
 void test_TEST_INT_EQ_eq(void)
 {
 	printf("%s\n", __func__);
@@ -37,7 +167,6 @@ void test_TEST_INT_EQ_not_eq(void)
 
 	FILE *devnull, *tmp;
 
-	// Execute with STDOUT redirected to /dev/null
 	devnull = fopen("/dev/null", "w");
 	tmp = stdout;
 	stdout = devnull;
@@ -56,7 +185,6 @@ void test_TEST_INT_EQ_not_eq_w_expr(void)
 
 	FILE *devnull, *tmp;
 
-	// Execute with STDOUT redirected to /dev/null
 	devnull = fopen("/dev/null", "w");
 	tmp = stdout;
 	stdout = devnull;
@@ -80,15 +208,6 @@ void test_TEST_INT_EQ_stacktrace(void)
 	char expectedstr[BUFFSIZE];
 	FILE *buffer, *tmp;
 
-	/* args:
-	 * 	- function name
-	 * 	- file name
-	 * 	- execution line number (i.e. when macro is called)
-	 * 	- first expression passed to macro
-	 * 	- second expression passed to macro
-	 * 	- integer value of first expression
-	 * 	- integer value of second expression
-	 */
 	snprintf(expectedstr, BUFFSIZE, \
 		"==========================================================\n"
 		"FAIL: %s\n"
@@ -97,11 +216,10 @@ void test_TEST_INT_EQ_stacktrace(void)
 		"\t\tTEST_INT_EQ(%s, %s)\n"
 		"\t\t\t%d != %d\n"
 		"----------------------------------------------------------\n", \
-		__FUNCTION__, __FILE__, __LINE__ + 8, "0", "1", 0, 1);
+		__FUNCTION__, __FILE__, __LINE__ + 7, "0", "1", 0, 1);
 	actualstr = calloc(BUFFSIZE, sizeof(char));
 	buffer = fmemopen(actualstr, BUFFSIZE, "w");
 
-	// Execute with STDOUT redirected to `actualstr`
 	tmp = stdout;
 	stdout = buffer;
 
@@ -131,11 +249,10 @@ void test_TEST_INT_EQ_stacktrace_w_expr(void)
 		"\t\tTEST_INT_EQ(%s, %s)\n"
 		"\t\t\t%d != %d\n"
 		"----------------------------------------------------------\n", \
-		__FUNCTION__, __FILE__, __LINE__ + 8, "-8+10", "8-10", 2, -2);
+		__FUNCTION__, __FILE__, __LINE__ + 7, "-8+10", "8-10", 2, -2);
 	actualstr = calloc(BUFFSIZE, sizeof(char));
 	buffer = fmemopen(actualstr, BUFFSIZE, "w");
 
-	// Execute with STDOUT redirected to `actualstr`
 	tmp = stdout;
 	stdout = buffer;
 
@@ -152,6 +269,13 @@ void test_TEST_INT_EQ_stacktrace_w_expr(void)
 int main(void)
 {
 	printf("--- %s ---\n", __FILE__);
+
+	test_TEST_TRUE_true();
+	test_TEST_TRUE_true_w_expr();
+	test_TEST_TRUE_false();
+	test_TEST_TRUE_false_w_expr();
+	test_TEST_TRUE_stacktrace();
+	test_TEST_TRUE_stacktrace_w_expr();
 
 	test_TEST_INT_EQ_eq();
 	test_TEST_INT_EQ_eq_w_expr();
