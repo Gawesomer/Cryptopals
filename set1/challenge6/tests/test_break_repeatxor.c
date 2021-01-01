8d7b61ed43158b69e7cad01a4a3b20d9db726531
Jean-Baptiste Jouband <jean-baptiste@jouband.com>
2021-01-01 16:20:22 -0600

    Make sure to free dynamic memory

--- set1/challenge6/tests/test_break_repeatxor.c
+++ set1/challenge6/tests/test_break_repeatxor.c
@@ -38,20 +38,30 @@ int test_get_block_n_oob_returns_null(void)
 
 int test_get_block_get_first_block(void)
 {
+	int status;
 	uint8_t arr[] = {1, 2, 3};
 	uint8_t expectedblock[] = {1, 2};
 	uint8_t *actualblock = get_block(arr, 3, 2, 0);
 
-	return TEST_BYTE_ARR_EQ(expectedblock, actualblock, 2);
+	status = TEST_BYTE_ARR_EQ(expectedblock, actualblock, 2);
+
+	free(actualblock);
+
+	return status;
 }
 
 int test_get_block_last_block_of_smaller_size(void)
 {
+	int status;
 	uint8_t arr[] = {1, 2, 3};
 	uint8_t expectedblock[] = {3};
 	uint8_t *actualblock = get_block(arr, 3, 2, 1);
 
-	return TEST_BYTE_ARR_EQ(expectedblock, actualblock, 1);
+	status = TEST_BYTE_ARR_EQ(expectedblock, actualblock, 1);
+
+	free(actualblock);
+
+	return status;
 }
 
 int main(void)
