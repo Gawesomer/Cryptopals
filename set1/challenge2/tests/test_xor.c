1629919db1da459f9af1937bc269e7c3eb6a719a
Jean-Baptiste Jouband <jj.jouband@gmail.com>
2021-01-04 19:35:31 -0600

    Refactor xor_binary

--- set1/challenge2/tests/test_xor.c
+++ set1/challenge2/tests/test_xor.c
@@ -5,170 +5,58 @@
 #include "cTest.h"
 #include "../xor.c"
 
-/*** xor_binarrays ***/
+/*** xor_binary ***/
 
-int test_xor_binarrays_null(void)
+int test_xor_binary_null(void)
 {
-	int status = 0;
 	uint8_t bits[] = {0xFF};
+	uint8_t orig[] = {0xFF};
 
-	status += TEST_BYTE_ARR_EQ(xor_binarrays(NULL, NULL, 0), NULL, 0);
-	status += TEST_BYTE_ARR_EQ(xor_binarrays(bits, NULL, 1), NULL, 0);
-	status += TEST_BYTE_ARR_EQ(xor_binarrays(NULL, bits, 1), NULL, 0);
+	xor_binary(bits, NULL, 1);
+	xor_binary(NULL, bits, 1);
 
-	return status;
+	return TEST_BYTE_ARR_EQ(bits, orig, 1);
 }
 
-int test_xor_binarrays_single_byte(void)
+int test_xor_binary_empty(void)
 {
-	int status;
+	uint8_t bits[] = {0xFF};
+	uint8_t orig[] = {0xFF};
+
+	xor_binary(bits, bits, 0);
+
+	return TEST_BYTE_ARR_EQ(bits, orig, 1);
+}
+
+int test_xor_binary_single_byte(void)
+{
 	uint8_t a[] = {0xFF};
 	uint8_t b[] = {0xAA};
 	uint8_t expected[] = {0x55};
-	uint8_t *actual = xor_binarrays(a, b, 1);
 
-	status = TEST_BYTE_ARR_EQ(expected, actual, 1);
+	xor_binary(a, b, 1);
 
-	free(actual);
-
-	return status;
+	return TEST_BYTE_ARR_EQ(expected, a, 1);
 }
 
-int test_xor_binarrays_two_bytes(void)
+int test_xor_binary_two_bytes(void)
 {
-	int status;
 	uint8_t a[] = {0x12, 0x34};
 	uint8_t b[] = {0x56, 0x78};
 	uint8_t expected[] = {0x44, 0x4C};
-	uint8_t *actual = xor_binarrays(a, b, 2);
 
-	status = TEST_BYTE_ARR_EQ(expected, actual, 2);
+	xor_binary(a, b, 2);
 
-	free(actual);
-
-	return status;
+	return TEST_BYTE_ARR_EQ(expected, a, 2);
 }
 
-/*** xor_hexstrs ***/
-
-int test_xor_hexstrs_null(void)
-{
-	int status = 0;
-
-	status += TEST_STR_EQ(xor_hexstrs(NULL, NULL), NULL);
-	status += TEST_STR_EQ(xor_hexstrs("a", NULL), NULL);
-	status += TEST_STR_EQ(xor_hexstrs(NULL, "b"), NULL);
-
-	return status;
-}
-
-int test_xor_hexstrs_empty(void)
-{
-	int status = 0;
-
-	status += TEST_STR_EQ(xor_hexstrs("", "b"), NULL);
-	status += TEST_STR_EQ(xor_hexstrs("a", ""), NULL);
-
-	return status;
-}
-
-int test_xor_hexstrs_single_bytes(void)
-{
-	int status;
-	char *res;
-
-	res = xor_hexstrs("FF", "AA");
-	status = TEST_STR_EQ(res, "55");
-
-	free(res);
-
-	return status;
-}
-
-int test_xor_hexstrs_results_in_letters(void)
-{
-	int status;
-	char *res;
-
-	res = xor_hexstrs("00", "BC");
-	status = TEST_STR_EQ(res, "BC");
-
-	free(res);
-
-	return status;
-}
-
-int test_xor_hexstrs_lowercase(void)
-{
-	int status;
-	char *res;
-
-	res = xor_hexstrs("ff", "aa");
-	status = TEST_STR_EQ(res, "55");
-
-	free(res);
-
-	return status;
-}
-
-int test_xor_hexstrs_different_sizes(void)
-{
-	int status = 0;
-	char *res;
-
-	res = xor_hexstrs("FFAA", "AAFFCC");
-	status += TEST_STR_EQ(res, "5555");
-	free(res);
-
-	res = xor_hexstrs("AAFFCC", "FFAA");
-	status += TEST_STR_EQ(res, "5555");
-	free(res);
-
-	return status;
-}
-
-int test_xor_hexstrs_odd_length(void)
-{
-	int status;
-	char *res;
-
-	res = xor_hexstrs("FFF", "AAA");
-	status = TEST_STR_EQ(res, "555");
-
-	free(res);
-
-	return status;
-}
-
-int test_xor_hexstrs_cryptopals_example(void)
-{
-	int status;
-	char a[] = "1c0111001f010100061a024b53535009181c";
-	char b[] = "686974207468652062756c6c277320657965";
-	char expected[] = "746865206B696420646F6E277420706C6179";
-	char *actual = xor_hexstrs(a, b);
-
-	status = TEST_STR_EQ(expected, actual);
-
-	free(actual);
-
-	return status;
-}
 
 int main(void)
 {
-	REGISTER_TEST(test_xor_binarrays_null);
-	REGISTER_TEST(test_xor_binarrays_single_byte);
-	REGISTER_TEST(test_xor_binarrays_two_bytes);
-
-	REGISTER_TEST(test_xor_hexstrs_null);
-	REGISTER_TEST(test_xor_hexstrs_empty);
-	REGISTER_TEST(test_xor_hexstrs_single_bytes);
-	REGISTER_TEST(test_xor_hexstrs_results_in_letters);
-	REGISTER_TEST(test_xor_hexstrs_lowercase);
-	REGISTER_TEST(test_xor_hexstrs_different_sizes);
-	REGISTER_TEST(test_xor_hexstrs_odd_length);
-	REGISTER_TEST(test_xor_hexstrs_cryptopals_example);
+	REGISTER_TEST(test_xor_binary_null);
+	REGISTER_TEST(test_xor_binary_empty);
+	REGISTER_TEST(test_xor_binary_single_byte);
+	REGISTER_TEST(test_xor_binary_two_bytes);
 
 	return RUN_TESTS();
 }
